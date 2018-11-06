9774789c958699c22c1cb2e3e24ef4e85a38317c
HaHaTa <minehahata@outlook.com>
2018-11-06 08:15:41 +0800

    bug fixed

--- exp1/viterbi_Euclid.m
+++ exp1/viterbi_Euclid.m
@@ -19,5 +19,22 @@ function [ans]=viterbi_Euclid(L,seq1,seq2,GF2n)
             ans=seq1(1);
         end
     else
+        if ((seq2(1)==0)&&(seq2(2)==0)&&(seq2(3)==0))
+            ans=seq1(5);
+        elseif ((seq2(1)==0)&&(seq2(2)==0)&&(seq2(3)==1))
+            ans=seq1(6);
+        elseif ((seq2(1)==0)&&(seq2(2)==1)&&(seq2(3)==0))
+            ans=seq1(8);
+        elseif ((seq2(1)==0)&&(seq2(2)==1)&&(seq2(3)==1))
+            ans=seq1(7);
+        elseif ((seq2(1)==1)&&(seq2(2)==0)&&(seq2(3)==0))
+            ans=seq1(3);
+        elseif ((seq2(1)==1)&&(seq2(2)==0)&&(seq2(3)==1))
+            ans=seq1(4);
+        elseif ((seq2(1)==1)&&(seq2(2)==1)&&(seq2(3)==0))
+            ans=seq1(2);
+        else
+            ans=seq1(1);
+        end
     end
 end
